5b8549ed978254f85f7df933cd1dc27852e9a32a
Karol Bonat <karol.bonatkarol@gmail.com>
2023-12-04 19:26:03 +0100

    Add hiding of Final report. Better color hiding.  Add more rules to color hiding. Hide the whole table of final report

--- content_scripts/scores.js
+++ content_scripts/scores.js
@@ -19,6 +19,7 @@
   
   const sioUrls=["*://*.sio2.staszic.waw.pl/*","*://*.sio2.mimuw.edu.pl/*", "*://*.szkopul.edu.pl/*"]
   const labelUrls=["sio2.staszic.waw.pl"]
+  const finalTestingKeywords = ["końcowe", "final"]
 
   const hideCSS=`.siohelper_hidden {
     display: none;
@@ -58,22 +59,57 @@
       if(elem.id.includes("submission") ){
         elem.style="background: #ffffff;" ;
       }
+      if(elem.classList.contains("submission") || elem.classList.contains("submission__margin") ){
+        elem.style="background: #ffffff;" ;
+      }
       if(elem.id.includes("score") ){
         elem.classList.add("siohelper_hidden");
       }
     }
   }
+
+  function hideFinalTestingReport(){
+    //Tables : powinna mieć dwa albo jeden element, jeden z nich to wstępne drugi to końcowe
+    var tables = document.getElementsByClassName("table-report")
+    //Ma tylko wstępne albo nic
+    if(tables.length < 2)
+      return;
+    var found = false;
+
+    for( table of tables ){
+      var a = table.parentElement.parentElement ;
+      for(c of a.children){
+        //Header
+        if(c.tagName[0] = 'H'){
+          str = c.textContent.toLowerCase()
+          for(key of finalTestingKeywords)
+            if(str.includes(key)) found=1
+        }
+      }
+      if(found){
+        table.parentElement.parentElement.classList.add("siohelper_hidden");
+        break;
+      }
+    }
+
+    //Hide score in top-submission view
+    var a = document.getElementById("submission-status-table")
+    var tr = a.getElementsByTagName("tbody")[0].children[0].children
+    // console.log(tr);
+    tr[tr.length-1].classList.add("siohelper_hidden");
+  }
         
   function hideScoresCall(){
     hideScoreBadges();
     hideScoreResults();
+    hideFinalTestingReport();
     browser.storage.local.set({"Hidden":true});
   }
 
   function showScores(){
     // browser.tabs.removeCSS({code: hidePage})
     
-    elems[i].classList.add("siohelper_hidden");
+    // elems[i].classList.add("siohelper_hidden");
     var elems = document.body.getElementsByTagName("*");
     for (let elem of elems){
       if(elem.id.includes("submission") ){
